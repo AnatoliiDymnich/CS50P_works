9737741dc7e6e7f58265a8fdcecf679a29cc7138
Anatolii Dymnich <staraw@ukr.net>
2023-07-25 12:24:05 +0300

    Problem Set 1

--- Problem Set 1/deep.py
+++ Problem Set 1/deep.py
@@ -1,3 +1,7 @@
+"""program prompts the user for the answer to the Great Question of Life, the Universe and Everything,
+outputting Yes if the user inputs 42 or (case-insensitively) forty-two or forty two.
+Otherwise output No."""
+
 answer = input("What is the Answer? ").strip().lower()
 
 match answer:
--- Problem Set 1/interpreter.py
+++ Problem Set 1/interpreter.py
@@ -1,3 +1,8 @@
+"""program prompts the user for an arithmetic expression and then calculates
+and outputs the result as a floating-point value formatted to one decimal place.
+Assume that the user's input will be formatted as x y z,
+with one space between x and y and one space between y and z"""
+
 expression = input("Expression: ")
 x, z, y = expression.split(" ")
 
